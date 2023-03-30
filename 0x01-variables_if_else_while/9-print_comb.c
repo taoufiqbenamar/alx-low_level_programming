49bffa74536575dcefa65772b150db33d969003f
taoufiqbenamar <taoufiqbenamarbritel@gmail.com>
2023-03-30 16:48:17 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 0)
+			putchar(i + '0');
+		else
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
