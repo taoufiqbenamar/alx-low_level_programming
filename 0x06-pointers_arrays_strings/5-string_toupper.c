73c40142d88fdf96d219f26ded748ba39aaa1973
taoufiqbenamar <taoufiqbenamarbritel@gmail.com>
2023-03-31 15:55:42 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,11 @@ char *string_toupper(char *n)
 	int i;
 
 	i = 0;
-	while (n[i] >= 'a' && n[i] <= 'z')
-		n[i] = n[i] - 32;
-	i++;
-}
-return (n);
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
 }
