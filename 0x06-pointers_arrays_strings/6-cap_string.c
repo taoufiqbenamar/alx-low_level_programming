4b4d9d8ba99044603588040f7ec79269c16a1637
taoufiqbenamar <taoufiqbenamarbritel@gmail.com>
2023-04-01 10:54:20 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,17 +8,29 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i, j;
+	int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
 
-	while (str[i])
+	i = 0;
+	while (*(str + i) != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		if (str[i - 1] == ' ' || str[i - 1] == 't' || str[i - 1] == 'n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
-			str[i] -= 32;
-
-		i++;
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(str + i) = *(str + i) - 32;
+			}
+			else
+			{
+				for (j = 0 ; j <= 12 ; j++)
+				{
+					if (a[j] == *(str + i - 1))
+					{
+						*(str + i) = *(str + i) - 32;
+					}
+				}
+			}
+		}
+	i++;
 	}
-
 	return (str);
-}
